750904c7627bcf6d231d2a75dfd2d23cc4881798
Alew <alejandrow@tmachine.com.uy>
2020-12-02 14:36:01 -0300

    Curso Udemy Patrones de diseño en Javascript y Node.JS. FIRST Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+class MiClase {
+    constructor(){
+        this.propiedad = 1
+        this.metodo = () => {
+    
+        }
+    }
+}
+
+
+const instancia = new MiClase();
+console.log(instancia);
