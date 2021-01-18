fd2e1f4d8921fe6753b8ef3aea9a0b6dfa7620e6
Alew <alejandrow@tmachine.com.uy>
2021-01-18 14:25:45 -0300

    Seccion 4 Patrones de estructura. Video 25 completo. Capitulo completo

---
+++ s4_patrones_de_estructura/s4_v25_adaptador.js
@@ -0,0 +1,54 @@
+class Api {
+    constructor(){
+        this.operations = function (url, opts, verb) {
+            switch (verb) {
+                case 'get':
+                    // return fetch
+                case 'post':
+                    // return fetch...
+                default:
+                    return ;
+                    
+            }
+        };
+    }
+}
+
+
+class Api2 {
+    constructor(){
+        this.get = function (url, opts) {
+            // return axios.get...
+        };
+        this.post = function (url, opts) {
+            // return axios.post...
+        };
+    }
+}
+
+// Adaptador
+class ApiAdapter {
+    constructor() {
+        const api2 = new Api2();
+
+        this.operations = function(url, opts, verb){
+            switch(verb){
+                case 'get':
+                    return api2.get(url, opts)
+                case 'post':
+                    return api2.post(url, opts)
+                default:
+                    return
+            }
+        };
+    };
+}
+
+const api = new Api();
+api.operations('www.google.com', {q:1},'get');
+
+const api2 = new Api2();
+api2.get('www.google.com', {q:1});
+
+const adapter = new ApiAdapter();
+adapter.operations('www.google.com', {q:1},'get');
